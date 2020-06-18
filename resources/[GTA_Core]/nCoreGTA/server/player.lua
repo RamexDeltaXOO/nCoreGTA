5f6b321e3501b9be77584758c432dbbf3b72eac1
Ninja <samir932205@gmail.com>
2020-06-18 17:33:12 +0200

    update version 1.3.3

--- resources/[GTA_Core]/nCoreGTA/server/player.lua
+++ resources/[GTA_Core]/nCoreGTA/server/player.lua
@@ -3,10 +3,10 @@ Player.__index = Player
 
 --> Version de la Resource : 
 local LatestVersion = ''; CurrentVersion = '1.3.3'
-PerformHttpRequest('https://raw.githubusercontent.com/NinjaSourceV2/nCoreGTA/master/VERSION', function(Error, NewestVersion, Header)
+PerformHttpRequest('https://raw.githubusercontent.com/NinjaSourceV2/nCoreGTA/master/resources/%5BGTA_Core%5D/nCoreGTA/VERSION?token=AG2XZ743TVCNKFACO4QHJV265OESQ', function(Error, NewestVersion, Header)
     LatestVersion = NewestVersion
     if CurrentVersion ~= NewestVersion then
-        print("\n\r ^2[GTA_Superette]^1 La version que vous utilisé n'est plus a jours, veuillez télécharger la dernière version. ^3\n\r")
+        print("\n\r ^2[nCoreGTA]^1 La version que vous utilisé n'est plus a jours, veuillez télécharger la dernière version. ^3\n\r")
     end
 end)
 
